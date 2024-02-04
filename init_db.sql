f901af8f15fa91535c2966a82e23cdbfcf3c59de
Roadway000 <roadway000@gmail.com>
2024-02-04 15:34:52 +0200

    sql files only

--- init_db.sql
+++ init_db.sql
@@ -1,5 +1,7 @@
 -- Завдання №1 - спроєктувати БД
 -- init_db.sql
+DROP TABLE IF EXISTS worker;
+
 create table worker(id bigint auto_increment not null primary key
 	, name varchar not null check(length(name)>2 and length(name)<1000)
 	, birthday DATETIME check(birthday>1900)
@@ -8,17 +10,24 @@ create table worker(id bigint auto_increment not null primary key
 );
 --create sequence seq_worker start 1
 
+DROP TABLE IF EXISTS client;
+
 create table client(id bigint auto_increment not null primary key
 	, name varchar not null check(length(name)>2 and length(name)<1000)
 );
 --create sequence seq_client start 1
 
+DROP TABLE IF EXISTS project;
+
 create table project(id bigint auto_increment not null primary key
 	, client_id bigint
 	, start_date datetime
 	, finish_date datetime
+	, foreign key (client_id) references client(id)
 );
+
 --create sequence seq_project start 1
+DROP TABLE IF EXISTS project_worker;
 
 create table project_worker(project_id bigint not null
 	, worker_id bigint not null
